23ae3f0461b5991c7b8355e4cc886348e42c95aa
NorthWestWind <wsyn148@gmail.com>
2021-07-01 08:03:01 +0800

    Fixed Obsidian Boots causing items unstackable

--- src/main/java/ml/northwestwind/moreboots/init/item/boots/ObsidianBootsItem.java
+++ src/main/java/ml/northwestwind/moreboots/init/item/boots/ObsidianBootsItem.java
@@ -18,10 +18,11 @@ public class ObsidianBootsItem extends BootsItem {
         EquipmentSlotType slot = event.getSlot();
         if (slot.equals(EquipmentSlotType.FEET)) return;
         LivingEntity entity = event.getEntityLiving();
+        ItemStack equipment = entity.getItemBySlot(slot);
+        if (!equipment.isDamageableItem()) return;
         ItemStack boots = entity.getItemBySlot(EquipmentSlotType.FEET);
         ItemStack to = event.getTo();
         ItemStack from = event.getFrom();
-        ItemStack equipment = entity.getItemBySlot(slot);
         int damage = to.getDamageValue() - from.getDamageValue();
         int maxRepair = boots.getMaxDamage() - boots.getDamageValue();
         if (maxRepair > damage) equipment.setDamageValue(0);
