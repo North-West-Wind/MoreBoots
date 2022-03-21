15f096ac600fbb6081f302dd07c0f7187a18c39b
NorthWestWind <wsyn148@gmail.com>
2022-03-21 15:16:48 +0800

    Slight tweaks with float up feature

--- src/main/java/ml/northwestwind/moreboots/init/item/boots/SuperAvianFeetItem.java
+++ src/main/java/ml/northwestwind/moreboots/init/item/boots/SuperAvianFeetItem.java
@@ -69,7 +69,7 @@ public class SuperAvianFeetItem extends BootsItem {
         if (!entity.hasEffect(MobEffects.MOVEMENT_SPEED) || entity.getEffect(MobEffects.MOVEMENT_SPEED).getAmplifier() < 1) entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 5, 0, false, false, false));
         if (!entity.isCrouching()) {
             double addToY = 0;
-            if (entity instanceof Player && ((MixinLivingEntityAccessor) entity).isJumping()) addToY = 0.02;
+            if (entity instanceof Player && entity.getDeltaMovement().y() < 0.02 &&  ((MixinLivingEntityAccessor) entity).isJumping()) addToY = 0.02;
             else if (entity.getDeltaMovement().y() < 0) addToY = -0.02;
             if (addToY != 0) {
                 entity.setDeltaMovement(entity.getDeltaMovement().multiply(1.05, 0, 1.05).add(0, addToY, 0));
