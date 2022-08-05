a1cfc2a43d7a07903056f3b3a84efb60066d66ab
yinzhi24 <yinzhi24@gmail.com>
2022-08-05 10:44:04 -0700

    Update MoreBootsConfig.java

--- src/main/java/ml/northwestwind/moreboots/config/MoreBootsConfig.java
+++ src/main/java/ml/northwestwind/moreboots/config/MoreBootsConfig.java
@@ -18,7 +18,7 @@ public class MoreBootsConfig {
         worn = builder.comment("Whether the viscousity is reversed for Bionic Beetle Feet. 0: Normal, 1: Reversed").defineInRange("bionic.worn", 0, 0, 1);
 
         abilities = builder.comment("Abilities of Bionic Beetle Feet. 0: Trailing, 1: Lightweight, 2: Slow falling, 3: Speed, 4: Swim, 5: Float, 6: Fast when no sprint, 7: Yoshi, 8: Build up speed for 12 hours to reach parallel universe, 9: Dash").defineList("bionic.abilities", Lists.newArrayList(0,1,2,3,4,5,6,7,8,9), o -> (o instanceof Integer ii) && ii >= 0 && ii <= 9);
-        aftermaths = builder.comment("Aftermath of Bionic Beetle Feet. Last for exactly 15 seconds. 0: Fast when no sprint, 1: Yoshi, 2: Jump boost, 3: Speed, 4: Slow falling, 5: Slippery, 6: Sticky, 7: Charge jumping, 8: Dash, 9: Swim faster, 10: Water/Lava/Power Snow Walker").defineList("bionic.aftermaths", Lists.newArrayList(0,1,2,3,4,5,6,7,8,9,10), o -> (o instanceof Integer ii) && ii >= 0 && ii <= 10);
+        aftermaths = builder.comment("Aftermath of Bionic Beetle Feet. Last for 15 seconds. 0: Fast when no sprint, 1: Yoshi, 2: Jump boost, 3: Speed, 4: Slow falling, 5: Slippery, 6: Sticky, 7: Charge jumping, 8: Dash, 9: Swim faster, 10: Water/Lava/Power Snow Walker").defineList("bionic.aftermaths", Lists.newArrayList(0,1,2,3,4,5,6,7,8,9,10), o -> (o instanceof Integer ii) && ii >= 0 && ii <= 10);
     }
 
     public int getViscousity() {
